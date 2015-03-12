ee992699a3a76acb77bbe532fae624a0cb0f2582
PeteyDog7 <bontkurrack@gmail.com>
2015-03-12 06:05:42 -0400

    added source package and main mod class

---
+++ src/main/java/com/peteydog7/mcstreamnotifier/MCStreamNotifier.java
@@ -0,0 +1,10 @@
+package com.peteydog7.mcstreamnotifier;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "mcstreamnotifier", name = "MC Stream Notifier", version = "1.7.10-1.0")
+public class MCStreamNotifier {
+
+
+
+}
